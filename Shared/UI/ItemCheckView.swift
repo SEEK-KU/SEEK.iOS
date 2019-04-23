8a5e03e9d512939d73a1661c0c2c9a3d30bab8eb
oatThanut <thanut.sjknk@gmail.com>
2019-04-23 16:12:37 +0700

    Update item list checking view state

--- Shared/UI/ItemCheckView.swift
+++ Shared/UI/ItemCheckView.swift
@@ -25,6 +25,15 @@ public class ItemCheckView: UIView
         get { return quantityTextField.text ?? "" }
         set { quantityTextField.text = newValue } }
     
+    public var isSelected: Bool {
+        get { return checkButton.isSelected }
+        set { checkButton.isSelected = newValue }
+    }
+    
+    public var isAllowEditing: Bool = false {
+        didSet { didSetViewState(oldValue) }
+    }
+    
     private let checkButton = UIButton()
     private let titleTextField = UITextField()
     private let quantityTextField = UITextField()
@@ -70,12 +79,15 @@ public class ItemCheckView: UIView
         
         titleTextField.borderStyle = .roundedRect
         titleTextField.placeholder = "ชื่ออาหาร/สิ่งของ&รายละเอียด"
+        titleTextField.isEnabled = false
         
         quantityTextField.borderStyle = .roundedRect
         quantityTextField.placeholder = "ชิ้น"
+        quantityTextField.isEnabled = false
         
         priceTextField.borderStyle = .roundedRect
         priceTextField.placeholder = "บาท"
+        priceTextField.isEnabled = false
     }
     
     // MARK: Constraints
@@ -113,7 +125,7 @@ public class ItemCheckView: UIView
                 $0.bottom.equalToSuperview()
                 $0.leading.equalTo(quantityTextField.snp.trailing).offset(8.0)
                 $0.trailing.equalToSuperview()
-                $0.width.equalTo(48.0)
+                $0.width.equalTo(72.0)
         }
     }
 }
@@ -129,5 +141,19 @@ extension ItemCheckView
                 return !lastState }
             .bind(to: checkButton.rx.isSelected)
             .disposed(by: disposeBag)
+    }
+    
+    func didSetViewState(_ oldValue: Bool)
+    {
+        if isAllowEditing
+        {
+            checkButton.setImage(#imageLiteral(resourceName: "icon-check"), for: .selected)
+            checkButton.isUserInteractionEnabled = true
+        }
+        else
+        {
+            checkButton.setImage(#imageLiteral(resourceName: "icon-check-disable"), for: .selected)
+            checkButton.isUserInteractionEnabled = false
+        }
     }
 }
